346ace958be9985e0f9afedac3b9e19cf7547186
sjkim <fmsj@naver.com>
2024-09-08 13:42:02 +0900

    .

--- components/pages/CreateSpec.tsx
+++ components/pages/CreateSpec.tsx
@@ -1,16 +1,25 @@
 "use client"
 
-import React, { useEffect, useState } from "react"
+import React, { useState } from "react"
 
-import { Button } from "@/components/ui/button"
+import { Badge } from "@/components/ui/badge"
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
 
 interface ClientSpecs {
-  cpuCores: number | string
+  cpuCores: string
   ram: string
   language: string
   platform: string
-  screenResolution: string
+  screenDisply: string
   colorDepth: number
   pixelRatio: number
   gpuInfo: string
@@ -108,16 +117,6 @@ const getOSInfo = (): string => {
   return "Unknown"
 }
 
-const getRAM = (): string => {
-  if ("deviceMemory" in navigator) {
-    const deviceMemory = (navigator as any).deviceMemory
-    if (deviceMemory < 1) return "Less than 1 GB"
-    if (deviceMemory >= 8) return "8 GB or more"
-    return `${deviceMemory} GB`
-  }
-  return "Unknown"
-}
-
 const getScreenInfo = (): string => {
   const width = window.screen.width * window.devicePixelRatio
   const height = window.screen.height * window.devicePixelRatio
@@ -132,59 +131,55 @@ const getScreenInfo = (): string => {
 
 export const CreateSpec = () => {
   const [clientSpecs, setClientSpecs] = useState<ClientSpecs>({
-    cpuCores: 0,
-    ram: "Unknown",
-    language: "",
-    platform: "",
-    screenResolution: "",
-    colorDepth: 0,
-    pixelRatio: 1,
-    gpuInfo: "Unknown",
-    browser: "Unknown",
-    operatingSystem: "Unknown",
-    cookiesEnabled: false,
-    doNotTrack: null,
-    online: false,
-    touchPoints: 0,
-    networkType: "Unknown",
-    networkSpeed: "Unknown",
+    cpuCores: "12 Thread",
+    ram: "",
+    language: navigator.language,
+    platform: navigator.platform,
+    screenDisply: getScreenInfo(),
+    colorDepth: window.screen.colorDepth,
+    pixelRatio: window.devicePixelRatio,
+    gpuInfo: getGPUInfo(),
+    browser: `${getBrowserInfo().browserName} ${getBrowserInfo().version}`,
+    operatingSystem: getOSInfo(),
+    cookiesEnabled: navigator.cookieEnabled,
+    doNotTrack: navigator.doNotTrack,
+    online: navigator.onLine,
+    touchPoints: navigator.maxTouchPoints,
+    networkType: getNetworkInfo().type,
+    networkSpeed: getNetworkInfo().speed,
     batteryStatus: "Unknown",
   })
 
-  const scanSpecs = async () => {
-    try {
-      const networkInfo = getNetworkInfo()
-      const browserInfo = getBrowserInfo()
-      const batteryStatus = await getBatteryStatus()
-      const osInfo = getOSInfo()
+  const [editableSpecs, setEditableSpecs] = useState({
+    cpuCores: "12 Thread",
+    ram: "",
+    screenDisply: getScreenInfo(),
+    operatingSystem: getOSInfo(),
+    gpu: "",
+  })
 
-      setClientSpecs({
-        cpuCores: navigator.hardwareConcurrency || "Unknown",
-        ram: getRAM(),
-        language: navigator.language,
-        platform: navigator.platform,
-        screenResolution: getScreenInfo(),
-        colorDepth: window.screen.colorDepth,
-        pixelRatio: window.devicePixelRatio,
-        gpuInfo: getGPUInfo(),
-        browser: `${browserInfo.browserName} ${browserInfo.version}`,
-        operatingSystem: osInfo,
-        cookiesEnabled: navigator.cookieEnabled,
-        doNotTrack: navigator.doNotTrack,
-        online: navigator.onLine,
-        touchPoints: navigator.maxTouchPoints,
-        networkType: networkInfo.type,
-        networkSpeed: networkInfo.speed,
-        batteryStatus: batteryStatus,
-      })
-    } catch (error) {
-      console.error("Error scanning specs:", error)
-    }
+  const [additionalInputs, setAdditionalInputs] = useState({
+    cpuAdditional: "",
+    DisplyAdditional: "",
+    gpuAdditional: "",
+    osAdditional: "",
+  })
+
+  const handleEditableChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEditableSpecs({
+      ...editableSpecs,
+      [e.target.name]: e.target.value,
+    })
   }
 
-  useEffect(() => {
-    scanSpecs()
-  }, [])
+  const handleAdditionalInputChange = (
+    e: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setAdditionalInputs({
+      ...additionalInputs,
+      [e.target.name]: e.target.value,
+    })
+  }
 
   const renderValue = (value: any) => {
     if (value === null || value === undefined) {
@@ -197,23 +192,127 @@ export const CreateSpec = () => {
   }
 
   return (
-    <div className="container mx-auto p-4">
-      <h1 className="text-2xl font-bold mb-4">Enhanced PC Spec Scanner</h1>
-      <Button onClick={scanSpecs} className="mb-4">
-        Rescan Specs
-      </Button>
-      <h2 className="text-xl font-bold mb-2">Client (Browser) Information:</h2>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mb-4">
-        {Object.entries(clientSpecs).map(([key, value]) => (
-          <Card key={key}>
-            <CardHeader>
-              <CardTitle>{key}</CardTitle>
-            </CardHeader>
-            <CardContent>
-              <p>{renderValue(value)}</p>
-            </CardContent>
-          </Card>
-        ))}
+    <div className="container mx-auto h-full p-4">
+      <h1 className="mb-4 text-2xl font-bold">Your PC Spec</h1>
+      <div className="mb-8">
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="w-[180px]">Spec</TableHead>
+              <TableHead className="">Value</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            <TableRow>
+              <TableCell className="font-medium">CPU</TableCell>
+              <TableCell>
+                <div className="flex items-center space-x-2">
+                  <Input
+                    name="cpuAdditional"
+                    value={additionalInputs.cpuAdditional}
+                    onChange={handleAdditionalInputChange}
+                    className="grow"
+                    placeholder="CPU 정보 입력"
+                  />
+                  <span className="min-w-[100px]">
+                    {editableSpecs.cpuCores}
+                  </span>
+                </div>
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell className="font-medium">Memory</TableCell>
+              <TableCell>
+                <Input
+                  name="ram"
+                  value={editableSpecs.ram}
+                  onChange={handleEditableChange}
+                  className="w-full"
+                  placeholder="메모리 정보 입력"
+                />
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell className="font-medium">Disply</TableCell>
+              <TableCell>
+                <div className="flex items-center space-x-2">
+                  <Input
+                    name="DisplyAdditional"
+                    value={additionalInputs.DisplyAdditional}
+                    onChange={handleAdditionalInputChange}
+                    className="grow"
+                    placeholder="디스플레이 정보 입력"
+                  />
+                  <span className="min-w-[100px]">
+                    {editableSpecs.screenDisply}
+                  </span>
+                </div>
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell className="font-medium">GPU</TableCell>
+              <div className="flex items-center space-x-2">
+                <Input
+                  name="GpuAdditional"
+                  value={additionalInputs.gpuAdditional}
+                  onChange={handleAdditionalInputChange}
+                  className="grow"
+                  placeholder="디스플레이 정보 입력"
+                />
+                <span className="min-w-[100px]">
+                  <TableCell>{clientSpecs.gpuInfo}</TableCell>
+                </span>
+              </div>
+            </TableRow>
+            <TableRow>
+              <TableCell className="font-medium">Browser</TableCell>
+              <TableCell>{clientSpecs.browser}</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell className="font-medium">OS</TableCell>
+              <TableCell>
+                <Input
+                  name="operatingSystem"
+                  value={editableSpecs.operatingSystem}
+                  onChange={handleEditableChange}
+                  className="w-full"
+                  placeholder="운영체제 정보 입력"
+                />
+              </TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </div>
+
+      <h2 className="mb-2 text-xl font-bold">Additional Information:</h2>
+      <div className="mb-4 grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+        {Object.entries(clientSpecs).map(([key, value]) => {
+          if (
+            ![
+              "cpuCores",
+              "ram",
+              "screenDisply",
+              "gpuInfo",
+              "browser",
+              "operatingSystem",
+            ].includes(key)
+          ) {
+            return (
+              <Card key={key}>
+                <CardHeader>
+                  <CardTitle>{key}</CardTitle>
+                </CardHeader>
+                <CardContent>
+                  <p>{renderValue(value)}</p>
+                </CardContent>
+              </Card>
+            )
+          }
+          return null
+        })}
+      </div>
+      <div className="fixed-bottom-right">
+        <Badge>PLTR갤러리</Badge>
       </div>
     </div>
   )
